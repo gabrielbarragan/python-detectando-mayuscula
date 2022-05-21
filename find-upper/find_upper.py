3d7f65973a2d3979eb80b99418eb70f66047cd35
gabrielbarragan <gabrielbarragan07@gmail.com>
2022-05-21 08:47:23 -0500

    Python detectando mayusculas terminado

--- find-upper/find_upper.py
+++ find-upper/find_upper.py
@@ -1,1 +1,14 @@
-"""code goes here"""
+from typing import Dict
+def find_upper(person: dict, person2: dict):
+    """"""
+
+
+    new_dict={}
+
+    if person['last_name']== person['last_name'].capitalize():
+        new_dict[person['last_name'][0]]= person['last_name']
+
+    if person2['last_name']== person2['last_name'].capitalize():
+        new_dict[person2['last_name'][0]]= person2['last_name']
+
+    return new_dict
